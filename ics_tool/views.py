3b7b5b74157793409bbd67ea931edf95a85451a4
vishnuvijay <vishnupdp@gmail.com>
2017-11-14 02:18:26 -0700

    Update views.py

--- ics_tool/views.py
+++ ics_tool/views.py
@@ -11,6 +11,9 @@ import datetime
 from django.http import HttpResponse
 import re
 
+from .forms import *
+from .models import *
+
 
 def health(request):
     return HttpResponse("3")
@@ -32,3 +35,53 @@ def index(request):
     print(form.errors)
 
     return render(request,'ics_tool/home.html',{})
+
+def add_donor(request):
+    template_name = 'ics_tool/add_donor.html'
+
+    if request.method == "GET":
+        return render(request, template_name)
+
+    form = AddDonorForm(request.POST)
+    if form.is_valid():
+      OrganizationName = request.POST.get('OrganizationName', '')
+      Salutation       = request.POST.get('Salutation', '')
+      FirstName        = request.POST.get('FirstName', '')
+      LastName         = request.POST.get('LastName', '')
+      Email            = request.POST.get('Email', '')
+      PhoneNumber      = request.POST.get('PhoneNumber', '')
+      Comments         = request.POST.get('Comments', '')
+      StreetAddress    = request.POST.get('StreetAddress', '')
+      City             = request.POST.get('City', '')
+      State            = request.POST.get('State', '')
+      Zip              = request.POST.get('Zip', '')
+      ICS              = request.POST.get('ICS', '')
+
+      LoadDonorObj = Donors(OrganizationName=OrganizationName,Salutation=Salutation,FirstName=FirstName,LastName=LastName,
+                            Email=Email,PhoneNumber=PhoneNumber,Comments=Comments,StreetAddress=StreetAddress,City=City,State=State,Zip=Zip,ICS=ICS)
+      LoadDonorObj.save()
+
+      return render(request,'ics_tool/donor_Success.html',{})
+
+    print(form.errors)
+
+    return render(request,'ics_tool/add_donor.html',{})
+
+def search_donor(request):
+
+    template_name = 'ics_tool/search_donor.html'
+
+    if request.method == "GET":
+        return render(request, template_name)
+
+    form = SearchDonorForm(request.POST)
+    if form.is_valid():
+      SearchQuery = request.POST.get('SearchQuery', '')
+
+      results = Donors.objects.filter(Q(FirstName__icontains=SearchQuery)).values()
+
+      return render(request,'ics_tool/donor_results.html',{'Results' : results})
+
+    print(form.errors)
+
+    return render(request,'ics_tool/search_donor.html',{})
