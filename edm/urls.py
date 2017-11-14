ced62bbb8278010f1fb03983c6145857aebce998
vishnuvijay <vishnupdp@gmail.com>
2017-11-14 02:39:39 -0700

    Update urls.py

--- edm/urls.py
+++ edm/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.core.urlresolvers import reverse
 
 from django.conf.urls.static import static
 from ics_tool import views
